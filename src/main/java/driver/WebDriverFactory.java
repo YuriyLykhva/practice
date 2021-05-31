3ca95ff332f1e79c98c4e82ae645db6c61387845
YuriyLykhva <yuriy.lykhva@gmail.com>
2021-05-31 15:49:40 +0300

    WebDriverFactory is fixed

--- src/main/java/driver/WebDriverFactory.java
+++ src/main/java/driver/WebDriverFactory.java
@@ -1,7 +1,6 @@
 package driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,37 +8,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class WebDriverFactory {
 
     private static WebDriver driver;
+    private static final BrowserEnum browser = BrowserEnum.CHROME;
 
-//    public static WebDriver getDriver(BrowserEnum browser) {
-//        if(driver == null) {
-//            System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-//            System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver.exe");
-//
-//            driver = initDriver(browser);
-//        }
-//        return driver;
-//    }
-//
-//    private static WebDriver initDriver(BrowserEnum browser) {
-//        switch (browser){
-//            case CHROME: return new ChromeDriver();
-//            case FIREFOX: return new FirefoxDriver();
-//            default: throw new RuntimeException("Browser not supported: " + browser);
-//        }
-//    }
         public static WebDriver getDriver() {
 
         if(driver == null) {
-            //TODO: create switch expression and connect with BrowserEnum
-            switch ("") {
-                case "FIREFOX": {
+            switch (browser) {
+                case CHROME: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                }
+                case FIREFOX: {
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
                 }
                 default: {
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    throw new RuntimeException("Browser not supported: " + browser);
                 }
             }
 
